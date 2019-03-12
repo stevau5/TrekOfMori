f2158245ed68830ef01eaab5beb1f70c13b419c0
stevau5 <stevenanthonyzanga@gmail.com>
2019-03-12 16:21:52 -0400

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D theRB;
+    public float movementSpeed;
+    public Animator myAnimator; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        theRB.velocity = movementSpeed * new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        myAnimator.SetFloat("MoveX", theRB.velocity.x);
+        myAnimator.SetFloat("MoveY", theRB.velocity.y);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1 || Input.GetAxisRaw("Horizontal") == -1)
+        {
+            myAnimator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnimator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# rpgGame
+
+Unity
