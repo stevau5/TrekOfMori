d9dc62e622c5a4eb82645ea1b463e69b1af9460e
Steven <stevenanthonyzanga@gmail.com>
2019-03-13 22:50:14 -0400

    added second game scene, must solve player duplication

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,22 @@ public class PlayerController : MonoBehaviour {
 
     public Rigidbody2D theRB;
     public float movementSpeed;
-    public Animator myAnimator; 
+    public Animator myAnimator;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        theRB.velocity = movementSpeed * new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        myAnimator.SetFloat("MoveX", theRB.velocity.x);
-        myAnimator.SetFloat("MoveY", theRB.velocity.y);
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(gameObject);
+    }
 
-        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1 || Input.GetAxisRaw("Horizontal") == -1)
-        {
-            myAnimator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
-            myAnimator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+    // Update is called once per frame
+    void Update () {
+        theRB.velocity = movementSpeed * new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+        myAnimator.SetFloat ("MoveX", theRB.velocity.x);
+        myAnimator.SetFloat ("MoveY", theRB.velocity.y);
+
+        if (Input.GetAxisRaw ("Horizontal") == 1 || Input.GetAxisRaw ("Vertical") == 1 || Input.GetAxisRaw ("Vertical") == -1 || Input.GetAxisRaw ("Horizontal") == -1) {
+            myAnimator.SetFloat ("lastMoveX", Input.GetAxisRaw ("Horizontal"));
+            myAnimator.SetFloat ("lastMoveY", Input.GetAxisRaw ("Vertical"));
         }
     }
 }
