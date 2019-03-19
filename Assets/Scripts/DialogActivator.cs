6a6ecdd54b7fb5a09a141d7ed3741ba323a41f48
Steven <stevenanthonyzanga@gmail.com>
2019-03-19 16:25:21 -0400

    commenting

--- Assets/Scripts/DialogActivator.cs
+++ Assets/Scripts/DialogActivator.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class DialogActivator : MonoBehaviour
 {
 
-    public string[] lines;
-    private bool canActivate;
-    public bool isPerson = true;
+    public string[] lines; //lines to be shown in dialog box
+    private bool canActivate; //boolean used to check whether a player has entered the collider zone
+    public bool isPerson = true; //check if the object is an npc or object
 
 
     // Start is called before the first frame update
@@ -19,17 +19,20 @@ public class DialogActivator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if play is in collider zone and clicks LMB then show dialog. 
         if(canActivate && Input.GetButtonDown("Fire1") && !DialogManager.instance.dialogBox.activeInHierarchy) {
             DialogManager.instance.showDialog(lines, isPerson);
         }
     }
 
+    //if player enters collider zone, then trigger canActivate to true
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player") {
             canActivate = true;
         }
     }
 
+    //if player enters collider zone, then trigger canActivate to false
     private void OnTriggerExit2D(Collider2D other) {
         if (other.tag == "Player") {
             canActivate = false;
--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -5,15 +5,15 @@ using UnityEngine.UI;
 
 
 public class DialogManager : MonoBehaviour {
-    public Text dialogText;
-    public Text nameText;
-    public GameObject dialogBox;
-    public GameObject nameBox;
+    public Text dialogText; //reference to dialog text within the dialog box
+    public Text nameText; // reference to the name text within the name box
+    public GameObject dialogBox; // reference to the dialog box when conversating with NPCs
+    public GameObject nameBox; // reference to the name Box during dialog sequences. 
 
-    public string[] dialogLines;
+    public string[] dialogLines; //array of dialog text 
 
-    public int currentLine;
-    private bool justStarted;
+    public int currentLine; //current line of dialog text. 
+    private bool justStarted; //check if a dialog sequence has just started, 
 
 
     public static DialogManager instance; 
@@ -29,41 +29,49 @@ public class DialogManager : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (dialogBox.activeInHierarchy) {  //is the dialog box active in current scene
-            if (Input.GetButtonUp("Fire1")) {
+            if (Input.GetButtonUp("Fire1")) { //when user clicks left mouse button
 
-                if (!justStarted) {
-                    currentLine++;
+                if (!justStarted) { //check if dialog just started, if not do this block
+                    currentLine++; // increase to the next line
 
-                    if (currentLine >= dialogLines.Length) {
-                        dialogBox.SetActive(false);
-                        PlayerController.instance.canMove = true;
+                    if (currentLine >= dialogLines.Length) { //making sure we dont go out of bounds array
+                        dialogBox.SetActive(false); // if we go over, take away the dialog box
+                        PlayerController.instance.canMove = true; // and allow our player to regain movement
 
                     }
                     else {
-                        checkIfName();
-                        dialogText.text = dialogLines[currentLine];
+                        checkIfName(); // check to see if the dialog line is actually a name
+                        dialogText.text = dialogLines[currentLine]; //change the dialog text to the next one
                     }
                 } else {
-                    justStarted = false;
+                    justStarted = false; //this is here to prevent the initial mouse click from jumping to the next dialog line. 
                 }
 
             }
         }
     }
 
-
-    public void showDialog(string[] newLines, bool isPerson) {
-        dialogLines = newLines;
-        currentLine = 0;
-        checkIfName();
-        dialogText.text = dialogLines[currentLine];
-        dialogBox.SetActive(true);
+    /**
+     *  show dialog activiates the dialog box on screen to show users text.
+     *  it akes newLines array as input which is what the npc or objects will say in conversation
+     *  isPerson checks whether the object you are talking to is an NPC or an object, depending
+     *  on the outcome, a name box wil or will not be displayed on top of the dialog box. 
+     * */
+    public void showDialog(string[] newLines, bool isPerson) { 
+        dialogLines = newLines; //diaolog lines are given new lines passed by character
+        currentLine = 0; //sets the initial line at 0. 
+        checkIfName(); //check if the name changes during conversation. 
+        dialogText.text = dialogLines[currentLine]; //assign the dialog text to the currentLine
+        dialogBox.SetActive(true); //set the dialog box as active. 
         justStarted = true;
-        nameBox.SetActive(isPerson);
-        PlayerController.instance.canMove = false;
+        nameBox.SetActive(isPerson); //check if isPerson is t/f and set the name box accordingly. 
+        PlayerController.instance.canMove = false; //halt the player from moving while in chat. 
 
     }
 
+    /**
+     * check if the dialog current line is actually a name that beloings in the name box. 
+     * */
     public void checkIfName() {
         if (dialogLines[currentLine].StartsWith("n-")) {
             nameText.text = dialogLines[currentLine].Replace("n-", "");
