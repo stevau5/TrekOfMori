82ccfb0b836b6e483f006363ac3714cb31393548
Steven <stevenanthonyzanga@gmail.com>
2019-03-19 21:33:42 -0400

    Added game Manager which controls game state, also the stat system is now fully functional

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+
+    public string charName;
+    public int playerLevel = 1;
+    public int currentEXP;
+    public int[] expToNextLevel;
+    public int maxLevel = 100;
+    public int baseEXP = 1000; 
+
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP = 100;
+    public int[] mpLvlBonus; 
+    public int strength;
+    public int defence;
+    public int weaponPower;
+    public int armorPower;
+    public string equippedWeapon;
+    public string equippedArmor;
+    public Sprite charImage; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        expToNextLevel = new int[maxLevel];
+        expToNextLevel[1] = baseEXP; 
+
+        for(int i = 2; i < expToNextLevel.Length; i++) {
+            expToNextLevel[i] = Mathf.FloorToInt(expToNextLevel[i-1] * 1.05f);
+        }
+
+
+        mpLvlBonus = new int[maxLevel];
+        mpLvlBonus[1] = 5;
+        for(int i = 2; i < mpLvlBonus.Length; i++) {
+            mpLvlBonus[i] = mpLvlBonus[i - 1] + 2; 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.K)) {
+            addExp(250);
+        }
+    }
+
+    public void addExp(int expToAdd) {
+        currentEXP += expToAdd;
+
+        if (playerLevel < maxLevel) {
+            if (currentEXP >= expToNextLevel[playerLevel]) {
+                currentEXP -= expToNextLevel[playerLevel];
+                playerLevel++;
+                Debug.Log("Congrats, you are now level: " + playerLevel);
+
+                //determine whether to add str or def .. based on odd or even.
+
+                if (playerLevel % 2 == 0) {
+                    strength++;
+                    Debug.Log("Congrats, your strength level is now: " + strength);
+
+                }
+                else {
+                    defence++;
+                    Debug.Log("Congrats, your defence level is now: " + defence);
+
+                }
+
+                maxHP = Mathf.FloorToInt(maxHP * 1.05f);
+                currentHP = maxHP;
+
+                maxMP += mpLvlBonus[playerLevel-1];
+                currentMP = maxMP;
+
+            }
+        }
+
+        if(playerLevel >= maxLevel) {
+            currentEXP = 0;
+        }
+
+    }
+
+
+}
--- Assets/Scripts/EssentialsLoader.cs
+++ Assets/Scripts/EssentialsLoader.cs
@@ -7,7 +7,7 @@ public class EssentialsLoader : MonoBehaviour
 
     public GameObject UIScreen;
     public GameObject player;
-
+    public GameObject gameMan; 
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +19,9 @@ public class EssentialsLoader : MonoBehaviour
         if (PlayerController.instance == null) {
             PlayerController clone = Instantiate(player).GetComponent<PlayerController>();
             PlayerController.instance = clone; 
+        }
+        if (GameManager.instance == null) {
+            Instantiate(gameMan);
         }
 
     }
